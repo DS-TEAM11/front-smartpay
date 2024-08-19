b0ac3c313dcf1408a4e96fd2f3a12fe9322cfd99
Haeil Lim <lhi7837@gmail.com>
2024-08-19 11:21:34 +0900

    style: 결제 완료 영수증 디자인 1차 완성 #5

--- src/component/Receipt.js
+++ src/component/Receipt.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
-
+import './Receipt.css';
+import axios from 'axios';
+import { Button } from 'bootstrap';
 // 로그인 정보 -> context 등에서 받아오기
 // 로그인 정보 + 쿼리스트링의 결제 순번
 // Spring Security에서 로그인 정보로 인가
 // 쿼리스트링의 값에 따라서 axios 요청 -> 받아온 데이터를 리액트에서 표시
 // 받아온 데이터를 출력하기
-const Receipt = () => {
+const Receipt = (props) => {
     const [formData, setFormData] = useState({
         name: '',
         amount: '',
@@ -25,22 +27,26 @@ const Receipt = () => {
     };
 
     return (
-        <div className="bg-gray-100">
-            <h1>결제 완료</h1>
-            <h2>2024.00.00. 00:00:00</h2>
-            <h2>주문 번호: (uuid)</h2>
-            <div>
-                <div className="title">GS25 - 동교점</div>
+        <div className="receipt">
+            {/* //component1 -> topInfo */}
+            <div className="topInfo">
+                <div className="title">결제 완료</div>
+                <div className="titleSubInfo">2024.00.00. 00:00:00</div>
+                <div className="titleSubInfo">주문 번호: (uuid)</div>
+            </div>
+            <div className="detailInfo bg-light d-flex flex-column w-95">
+                <div className="purchaseShopName">GS25 - 동교점</div>
                 <hr />
                 <div className="purchaseItemName">참깨라면(대컵) 외 24종</div>
-                <div className="purchaseItemPrice d-grid">
+                <div className="purchaseItemInfo">
+                    {/* axios로 받아온 값을 각각 infoTitle, infoValue로 나눠서 작성 */}
                     <div className="purchaseItemPriceName">
                         <div className="infoTitle">결제 금액</div>
                         <div className="infoValue">150,000원</div>
                     </div>
                     <div className="purchaseBenefit">
                         <div className="infoTitle">받은 혜택</div>
-                        <div className="infoValue">300원</div>
+                        <div className="infoValue royalblue">300원</div>
                     </div>
                     <div className="purchaseUsedCard">
                         <div className="infoTitle">결제 카드</div>
@@ -55,6 +61,7 @@ const Receipt = () => {
                         <div className="infoValue">본인</div>
                     </div>
                 </div>
+                <button> 돌아가기 </button>
             </div>
         </div>
     );

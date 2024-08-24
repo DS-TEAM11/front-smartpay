0f99d6987ababe76a1cc2e26cfdba9d0a0c9a3d2
Haeil Lim <lhi7837@gmail.com>
2024-08-24 19:32:13 +0900

    fix: InputValue 수정 #27
    React.memo 사용, 리렌더링 발생하지 않게 수정

--- src/component/common/InputValue.js
+++ src/component/common/InputValue.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Button from '../Button';
 
-const InputValue = ({ title, ...props }) => {
+const InputValue = React.memo(({ title, ...props }) => {
     const { placeholder, id, onChange, readOnly, value, type, disabled } =
         props;
     const inputProps = {
@@ -24,36 +24,39 @@ const InputValue = ({ title, ...props }) => {
             ) : (
                 ''
             )}
-            <br />
             <input {...inputProps} />
         </div>
     );
-};
+});
 
-const InputValueWithBtn = ({ title, onClick, ...props }) => {
-    const { placeholder, id, onChange, readOnly, value, type, disabled } =
-        props;
-    const inputProps = {
-        id,
-        type: type || 'text',
-        className: 'inputValue form-control',
-        value,
-        placeholder,
-        onChange,
-        ...(readOnly ? { readOnly: true } : {}), // readOnly가 true일 때만 추가
-        ...(disabled ? { disabled: true } : {}), // disabled가 true일 때만 추가
-    };
+const InputValueWithBtn = React.memo(
+    ({ title, onClick, btnText, ...props }) => {
+        const { placeholder, id, onChange, readOnly, value, type, disabled } =
+            props;
+        const inputProps = {
+            id,
+            type: type || 'text',
+            className: 'inputValue form-control',
+            value: value,
+            placeholder,
+            onChange,
+            ...(readOnly ? { readOnly: true } : {}), // readOnly가 true일 때만 추가
+            ...(disabled ? { disabled: true } : {}), // disabled가 true일 때만 추가
+        };
 
-    return (
-        <div className="form-group">
-            <label className="form-label" htmlFor={id}>
-                {title}
-            </label>
-            <br />
-            <input {...inputProps} />
-            <Button text={'등록'} onClick={onClick} />
-        </div>
-    );
-};
-// , React.memo(InputValueWithBtn) };
-export default React.memo(InputValue);
+        return (
+            <div className="form-group">
+                {title ? (
+                    <label className="form-label" htmlFor={id}>
+                        {title}
+                    </label>
+                ) : (
+                    ''
+                )}
+                <input {...inputProps} />
+                <Button text={btnText} onClick={onClick} />
+            </div>
+        );
+    },
+);
+export { InputValue, InputValueWithBtn };

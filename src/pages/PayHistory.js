ec0d131f8de289096ade805340c4105763e8e402
Haeil Lim <lhi7837@gmail.com>
2024-08-25 22:30:40 +0900

    fix: merge 오류 수정

--- src/pages/PayHistory.js
+++ src/pages/PayHistory.js
@@ -175,7 +175,7 @@ function PayHistory() {
                                                 {payment.franchiseName}
                                             </div>
                                             <div className="div3">
-                                                {payment.save_type === 0
+                                                {payment.saveType === 0
                                                     ? `${formatCurrency(
                                                           payment.savePrice,
                                                       )} 적립`

f534e3e41279ed8975bfe0d2ff92af9d8f4ed206
chomyeonggeun <lightkeun@gmail.com>
2024-08-26 16:01:38 +0900

    fix: 달력 조회일 수정 및 메달표시 #3

--- src/component/MyCalendar.js
+++ src/component/MyCalendar.js
@@ -1,49 +1,70 @@
 import React, { useState, useEffect } from 'react';
 import Calendar from 'react-calendar';
-import Button from './Button';
 import 'react-calendar/dist/Calendar.css';
 import './MyCalendar.css'; // 추가된 스타일 시트
 
 function MyCalendar({ value, onChange }) {
     const [showCalendar, setShowCalendar] = useState(false);
-    const [selectedDate, setSelectedDate] = useState(value);
+    const [selectedDateRange, setSelectedDateRange] = useState(value);
 
     useEffect(() => {
-        setSelectedDate(value);
-    }, [value]);
+        if (!value) {
+            const today = new Date();
+            const firstDayOfMonth = new Date(
+                today.getFullYear(),
+                today.getMonth(),
+                1,
+            );
+            const lastDayOfMonth = new Date(
+                today.getFullYear(),
+                today.getMonth() + 1,
+                0,
+            );
+            const defaultRange = [firstDayOfMonth, lastDayOfMonth];
+            setSelectedDateRange(defaultRange);
+            onChange(defaultRange); // 부모 컴포넌트에 기본 날짜 범위 전달
+        } else {
+            setSelectedDateRange(value);
+        }
+    }, [value, onChange]);
 
     const toggleCalendar = () => {
         setShowCalendar(!showCalendar);
     };
 
-    const handleDateChange = (date) => {
-        setSelectedDate(date);
-        onChange(date); // 날짜가 변경되면 부모 컴포넌트에 알림
+    const handleDateChange = (dateRange) => {
+        setSelectedDateRange(dateRange);
+        onChange(dateRange); // 날짜 범위가 변경되면 부모 컴포넌트에 알림
     };
 
-    const formatDate = (date) => {
-        if (!date) return '날짜 선택';
-        const year = date.getFullYear();
-        const month = String(date.getMonth() + 1).padStart(2, '0');
-        const day = String(date.getDate()).padStart(2, '0');
-        return `${year}-${month}-${day}`;
+    const formatDateRange = (dateRange) => {
+        if (!dateRange || dateRange.length < 2) return '날짜 범위 선택';
+
+        const [startDate, endDate] = dateRange;
+
+        const format = (date) => {
+            if (!(date instanceof Date)) return '날짜선택';
+            const year = date.getFullYear();
+            const month = String(date.getMonth() + 1).padStart(2, '0');
+            const day = String(date.getDate()).padStart(2, '0');
+            return `${year}-${month}-${day}`;
+        };
+
+        return `${format(startDate)} ~ ${format(endDate)}`;
     };
 
     return (
         <div className="mycalendar">
             <button className="btn" onClick={toggleCalendar}>
-                <i class="bi bi-calendar4"></i>{' '}
-                {selectedDate ? formatDate(selectedDate) : '최근 일주일'}
+                <i className="bi bi-calendar4"></i>{' '}
+                {formatDateRange(selectedDateRange)}
             </button>
             {showCalendar && (
                 <div className="calendar-container">
                     <Calendar
                         onChange={handleDateChange}
-                        value={
-                            selectedDate instanceof Date
-                                ? selectedDate
-                                : new Date()
-                        }
+                        value={selectedDateRange}
+                        selectRange={true} // 날짜 범위 선택 모드 활성화
                     />
                 </div>
             )}

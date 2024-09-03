e6f6034dfd0fab6293a30b72262e72a827066197
chomyeonggeun <lightkeun@gmail.com>
2024-09-03 10:19:14 +0900

    feat: 무한스크롤 #3

--- src/pages/PayHistory.js
+++ src/pages/PayHistory.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import './PayHistory.css';
 import Header from '../component/Header';
@@ -12,6 +12,13 @@ function PayHistory() {
     const memberNo = useMemberNo();
     const [cardNo, setCardNo] = useState('');
     const [isRotated, setIsRotated] = useState(false);
+    const [page, setPage] = useState(1); // 페이지 번호 상태 추가
+    const [size] = useState(10); // 페이지 크기 설정
+    const [hasMore, setHasMore] = useState(true); // 추가 데이터가 있는지 여부
+
+    const observer = useRef(); // IntersectionObserver를 위한 ref
+
+    const lastElementRef = useRef(); // 마지막 요소를 추적하기 위한 ref
 
     const handleRotateChange = (rotated, parentDiv, imgElement) => {
         setIsRotated(rotated);
@@ -20,6 +27,7 @@ function PayHistory() {
             parentDiv.style.width = imgElement.height + 'px';
         }
     };
+
     // 현재 달의 시작일과 종료일 계산
     const today = new Date();
     const firstDayOfMonth = new Date(today.getFullYear(), today.getMonth(), 1);
@@ -29,7 +37,6 @@ function PayHistory() {
         0,
     );
 
-    // 기본 시작일과 종료일을 상태로 설정
     const [dateRange, setDateRange] = useState([
         firstDayOfMonth,
         lastDayOfMonth,
@@ -37,21 +44,21 @@ function PayHistory() {
 
     useEffect(() => {
         if (memberNo) {
-            paymentApi(dateRange, cardNo); // 초기 렌더링 시 결제 내역 호출
+            paymentApi(dateRange, cardNo, page, size); // 초기 렌더링 시 결제 내역 호출
         }
-    }, [memberNo]);
+    }, [memberNo, page]);
 
-    const paymentApi = (dateRange, cardNo) => {
-        // dateRange를 YYYYMMDD 형식으로 변환
+    const paymentApi = (dateRange, cardNo, page, size) => {
         const [startDate, endDate] = dateRange.map((date) => formatDate(date));
 
         axios
             .get(
-                `http://localhost:8091/api/payment/history?memberNo=${memberNo}&cardNo=${cardNo}&startDate=${startDate}&endDate=${endDate}`,
+                `http://localhost:8091/api/payment/history?memberNo=${memberNo}&cardNo=${cardNo}&startDate=${startDate}&endDate=${endDate}&page=${page}&size=${size}`,
             )
             .then((res) => {
                 console.log(res.data);
-                setPaymentData(res.data); // 결제 데이터 저장
+                setPaymentData((prevData) => [...prevData, ...res.data]); // 결제 데이터 저장
+                setHasMore(res.data.length > 0); // 추가 데이터가 더 있는지 확인
             })
             .catch((e) => {
                 console.error('결제 데이터 요청 에러', e);
@@ -74,10 +81,14 @@ function PayHistory() {
 
     const onChangeDateRange = (dateRange) => {
         setDateRange(dateRange);
+        setPage(1); // 날짜 범위 변경 시 페이지 번호 초기화
+        setPaymentData([]); // 기존 데이터 초기화
     };
 
     const onChangeCardNo = (e) => {
         setCardNo(e.target.value);
+        setPage(1); // 카드 번호 변경 시 페이지 번호 초기화
+        setPaymentData([]); // 기존 데이터 초기화
     };
 
     useEffect(() => {
@@ -86,41 +97,47 @@ function PayHistory() {
 
     useEffect(() => {
         if (dateRange.length > 0 || cardNo) {
-            paymentApi(dateRange, cardNo); // 날짜 범위 또는 카드 번호가 변경될 때 결제 데이터를 불러옵니다.
+            paymentApi(dateRange, cardNo, page, size); // 날짜 범위 또는 카드 번호가 변경될 때 결제 데이터를 불러옵니다.
         }
-    }, [dateRange, cardNo]);
+    }, [dateRange, cardNo, page]);
 
-    // Date 객체를 'YYYYMMDD' 문자열로 변환하는 함수
+    useEffect(() => {
+        if (observer.current) observer.current.disconnect();
+        observer.current = new IntersectionObserver((entries) => {
+            if (entries[0].isIntersecting && hasMore) {
+                setPage((prevPage) => prevPage + 1); // 페이지 번호 증가
+            }
+        });
+        if (lastElementRef.current)
+            observer.current.observe(lastElementRef.current);
+    }, [lastElementRef.current, hasMore]);
+
     const formatDate = (date) => {
         const year = date.getFullYear();
         const month = String(date.getMonth() + 1).padStart(2, '0');
         const day = String(date.getDate()).padStart(2, '0');
-        return `${year}${month}${day}`; // YYYYMMDD 형식
+        return `${year}${month}${day}`;
     };
 
-    // 'YYYYMMDD' 형식의 날짜를 "월 일 요일" 형식으로 변환
     const formatDayName = (dateString) => {
         const date = new Date(
-            dateString.slice(0, 4), // 연도
-            dateString.slice(4, 6) - 1, // 월 (0부터 시작)
-            dateString.slice(6, 8), // 일
+            dateString.slice(0, 4),
+            dateString.slice(4, 6) - 1,
+            dateString.slice(6, 8),
         );
 
         return date.toLocaleDateString('ko-KR', {
-            month: 'long', // "8월"
-            day: 'numeric', // "26일"
-            weekday: 'long', // "월요일"
+            month: 'long',
+            day: 'numeric',
+            weekday: 'long',
         });
     };
 
-    // 숫자를 통화 형식으로 변환하는 함수 (예: 1000 -> '1,000원')
     const formatCurrency = (amount) => {
-        // amount가 null 또는 undefined일 경우 0으로 처리
         const validAmount = amount ?? 0;
         return `${validAmount.toLocaleString()}원`;
     };
 
-    // 결제 데이터를 날짜별로 그룹화하는 함수
     const groupByDayName = (data) => {
         return data.reduce((acc, payment) => {
             const dayName = formatDayName(payment.payDate);
@@ -170,51 +187,64 @@ function PayHistory() {
                             {Object.keys(groupedData).map((dayName) => (
                                 <div className="dayTable" key={dayName}>
                                     <div className="dayName">{dayName}</div>
-                                    {groupedData[dayName].map((payment) => (
-                                        <div
-                                            key={payment.orderNo}
-                                            className="table-row cssportal-grid border-bottom"
-                                        >
-                                            <div className="item1">
-                                                <span className='franchiseName'>                                                    
-                                                    {payment.franchiseName}
-                                                </span>                                                
-                                            </div>
-                                            <div className="item2">
-                                                <CardImg
-                                                    className="small"
-                                                    src={payment.cardImage}
-                                                    alt="카드이미지"
-                                                    direction="horizontal"
-                                                    onRotateChange={
-                                                        handleRotateChange
-                                                    }
-                                                />
-                                                <div>
-                                                    {payment.cardName}
+                                    {groupedData[dayName].map(
+                                        (payment, index) => (
+                                            <div
+                                                key={payment.orderNo}
+                                                className="table-row cssportal-grid border-bottom"
+                                                ref={
+                                                    index ===
+                                                    groupedData[dayName]
+                                                        .length -
+                                                        1
+                                                        ? lastElementRef
+                                                        : null
+                                                }
+                                            >
+                                                <div className="item1">
+                                                    <span className="franchiseName">
+                                                        {payment.franchiseName}
+                                                    </span>
+                                                </div>
+                                                <div className="item2">
+                                                    <CardImg
+                                                        className="small"
+                                                        src={payment.cardImage}
+                                                        alt="카드이미지"
+                                                        direction="horizontal"
+                                                        onRotateChange={
+                                                            handleRotateChange
+                                                        }
+                                                    />
+                                                    <div>
+                                                        {payment.cardName}
+                                                    </div>
+                                                </div>
+                                                <div className="item3">
+                                                    {payment.getIsAi ===
+                                                    true ? (
+                                                        <>
+                                                            {payment.saveType ===
+                                                            0
+                                                                ? `${formatCurrency(
+                                                                      payment.savePrice,
+                                                                  )} 적립`
+                                                                : `${formatCurrency(
+                                                                      payment.savePrice,
+                                                                  )} 할인`}
+                                                        </>
+                                                    ) : (
+                                                        ''
+                                                    )}
+                                                </div>
+                                                <div className="item4">
+                                                    {formatCurrency(
+                                                        payment.price,
+                                                    )}
                                                 </div>
                                             </div>
-                                            <div className="item3">
-                                                {/* getIsAi가 true일 때만 적립 또는 할인 정보 표시 */}
-                                                {payment.getIsAi === true ? (
-                                                    <>
-                                                        {payment.saveType === 0
-                                                            ? `${formatCurrency(
-                                                                  payment.savePrice,
-                                                              )} 적립`
-                                                            : `${formatCurrency(
-                                                                  payment.savePrice,
-                                                              )} 할인`}
-                                                    </>
-                                                ) : (
-                                                    ''
-                                                )}
-                                            </div>
-                                            <div className="item4">
-                                                {formatCurrency(payment.price)}
-                                            </div>
-                                        </div>
-                                    ))}
+                                        ),
+                                    )}
                                 </div>
                             ))}
                         </div>
